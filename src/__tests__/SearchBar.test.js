a2e947fd9000dd6837ec773879ff0264e4fc922e
Filipe <ff467447@gmail.com>
2023-05-09 21:08:04 -0300

    Reformulação do README e fix de responsividade

--- src/__tests__/SearchBar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { SearchBar } from '../components/SearchBar/SearchBar';
-
-test('Se a search bar esta sendo renderizada', () => {
-    render(<SearchBar />);
-    const searchElement = screen.getByRole('textbox');
-    expect(searchElement).toBeInTheDocument();
-});
--- src/__tests__/sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require("../components/teste.jsx/sum");
-
-test('Soma de 1 + 2 é igual 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
